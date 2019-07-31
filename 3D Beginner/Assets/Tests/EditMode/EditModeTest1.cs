73895356b0015caf01826d9f420ccb967a19f992
yirui wang <yirui.wang@unity3d.com>
2019-07-31 10:15:10 +0800

    ?

--- 3D Beginner/Assets/Tests/EditMode/EditModeTest1.cs
+++ 3D Beginner/Assets/Tests/EditMode/EditModeTest1.cs
@@ -13,6 +13,9 @@ namespace Tests
         public void EditModeTest1SimplePasses()
         {
             // Use the Assert class to test conditions
+
+            bool isActive = false;
+            Assert.AreEqual(false, isActive);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -22,13 +25,7 @@ namespace Tests
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
-            var go = new GameObject();
-            go.AddComponent<Rigidbody>();
-            var originalPosition = go.transform.position.y;
-
-            yield return new WaitForFixedUpdate();
-
-            Assert.AreNotEqual(originalPosition, go.transform.position.y);
+            yield return null;
         }
     }
 }
